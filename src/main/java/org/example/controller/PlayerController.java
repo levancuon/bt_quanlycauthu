da5159807e557b475e43f66d55c134fc18a6ef9b
levancuon <levancuongprovip@gmail.com>
2024-07-16 15:20:41 +0700

    messi

---
+++ src/main/java/org/example/controller/PlayerController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/player")
+public class PlayerController {
+    @GetMapping("")
+    public String showForm(){
+        return "/info_player";
+    }
+}
